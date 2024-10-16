fe0a9b3583630b653dc886aee4e683bf2d857104
kali <kali@parrotsec>
2024-10-16 18:22:30 +0600

    try3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const app = express();
 app.use(cors());
 app.use(express.json());
-const app = express();
 const port = process.env.PORT || 5000;
 app.get('/',(req, res)=>{
     res.send("Hello World")
